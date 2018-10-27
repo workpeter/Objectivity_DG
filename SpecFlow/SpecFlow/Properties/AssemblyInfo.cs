f61c5731c63e103edf881388b461bca0b503f5ce
Peter Anderson <peter.x4000@gmail.com>
2018-10-27 12:11:14 +0100

    Cleanup

--- SpecFlow/SpecFlow/Properties/AssemblyInfo.cs
+++ SpecFlow/SpecFlow/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApp3")]
+[assembly: AssemblyTitle("SpecFlow")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp3")]
+[assembly: AssemblyProduct("SpecFlow")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8bb5bebb-9c67-4cad-bb1a-236309e4a9b2")]
+[assembly: Guid("d21b35da-767e-4a10-9938-455841ace500")]
 
 // Version information for an assembly consists of the following four values:
 //
